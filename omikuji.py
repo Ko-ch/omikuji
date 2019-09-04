69bfa86adbf29f8d383224f5ed3b3bb7e85bb71a
Koichiro <koichiro_takagi@outlook.jp>
2019-09-04 23:23:31 +0900

    complete application

---
+++ omikuji.py
@@ -0,0 +1,12 @@
+
+def unsei(i):
+    if i == 0:
+        return "大吉"
+    if i == 1:
+        return "吉"
+    if i == 2:
+        return "凶"
+
+import random
+
+print('今日の運勢は... '+unsei(random.randint(0,2)))
