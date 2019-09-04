ff298dac2d518e008cda196bde1968415a766b99
Koichiro <koichiro_takagi@outlook.jp>
2019-09-04 23:32:34 +0900

    add omikuji

--- omikuji.py
+++ omikuji.py
@@ -2,10 +2,29 @@ def unsei(i):
     if i == 0:
         return "大吉"
     if i == 1:
-        return "吉"
+        return "中吉"
     if i == 2:
+        return "小吉"
+    if i == 3:
+        return "吉"
+    if i == 4:
+        return "半吉"
+    if i == 5:
+        return "末吉"
+    if i == 6:
+        return "末小吉"
+    if i == 7:
         return "凶"
+    if i == 8:
+        return "小凶"
+    if i == 9:
+        return "半凶"
+    if i == 10:
+        return "末凶"
+    if i == 11:
+        return "大凶"
 
 import random
 
-print('今日の運勢は... ' + unsei(random.randint(0, 2)))
+for i in 100:
+    print('今日の運勢は... ' + unsei(random.randint(0, 11)))
